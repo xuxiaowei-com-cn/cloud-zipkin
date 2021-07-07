bbca34f07d2ec13a2ecdc329cf49bf7b438a38d3
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-07-07 18:41:39 +0800

    init

---
+++ src/main/java/cn/com/xuxiaowei/zipkin/CloudZipkinApplication.java
@@ -0,0 +1,18 @@
+package cn.com.xuxiaowei.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序执行入口
+ *
+ * @author xuxiaowei
+ */
+@SpringBootApplication
+public class CloudZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudZipkinApplication.class, args);
+    }
+
+}
